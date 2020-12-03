ae7d90f0ce3bedb33ce6cbfe39b2aaee6fa1765b
遠竹悠 <hiroshi_totake@r.recruit.co.jp>
2020-12-04 00:15:17 +0900

    練習問題9.2

--- ch09/ex01/main.go
+++ ch09/ex01/main.go
@@ -5,7 +5,7 @@
 //!+
 
 // Package bank provides a concurrency-safe bank with one account.
-package bank
+package main
 
 var deposits = make(chan int) // send amount to deposit
 var balances = make(chan int) // receive balance
